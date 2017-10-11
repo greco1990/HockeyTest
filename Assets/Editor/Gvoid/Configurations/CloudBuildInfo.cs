7e8f41d58c772a045c663dd6a5633a5a21a91d91
greco1990 <qqsholmesmaster13@gmail.com>
2017-10-12 03:09:51 +0900

    sec

--- Assets/Editor/Gvoid/Configurations/CloudBuildInfo.cs
+++ Assets/Editor/Gvoid/Configurations/CloudBuildInfo.cs
@@ -2,10 +2,10 @@ namespace Editor.Gvoid.Configurations
 {
     public static class CloudBuildInfo
     {
-        public static string CloudBuildApiKey = "CloudBuildApiKeyggg";
-        public static string OrganizationId = "OrganizationId";
-        public static string ProjectId = "ProjectId";
+        public static string CloudBuildApiKey = "048a4383818677c402c5a4198fa137f6";
+        public static string OrganizationId = "dasacdcdc";
+        public static string ProjectId = "2ddca310-6e3f-4a7d-9a87-4d738f6f93de";
         public static string iOSBuildTargetId = "ios buildtargetid";
-        public static string AndroidBuildTargetId = "android buildtargetid";
+        public static string AndroidBuildTargetId = "jp.co.testcompany.testapp";
     }
 }
--- Assets/Editor/Gvoid/Configurations/HockeyAppInfo.cs
+++ Assets/Editor/Gvoid/Configurations/HockeyAppInfo.cs
@@ -2,8 +2,8 @@
 {
     public static class HockeyAppInfo
     {
-        public static string HockeyappAppID_Android = "AppID Android";
+        public static string HockeyappAppID_Android = "79f8435b05124f218e7f54859a083abe";
         public static string HockeyappAppID_iOS = "AppID iOS";
-        public static string HockeyappApiKey = "Hockeyapp Api Key";
+        public static string HockeyappApiKey = "3ccbcbdbdd4b4700a4e489cf9c81d317";
     }
 }
